dd2a0e665384157e02ace5e920af78f07d3b6cb1
joonsong lee <ytytjkh@gmail.com>
2026-04-09 12:21:41 +0900

    Fix calculation of num_layers in overcooked.py

--- jaxmarl/environments/overcooked_v2/overcooked.py
+++ jaxmarl/environments/overcooked_v2/overcooked.py
@@ -511,7 +511,7 @@ class OvercookedV2(MultiAgentEnv):
                 case ObservationType.DEFAULT:
                     num_ingredients = self.layout.num_ingredients
                     #17(Invariable objects[5 position and directions for each agents, wall, goal, pot, plate pile, recipe indicator, button, pot timer] + 4*(num_ingredients+2) objects[Inventory for each agents, recipe, grid] + N(ingredient pile))
-                    num_layers = 17 + N + 4 * (num_ingredients + 2)
+                    num_layers = 17 + num_ingredients + 4 * (num_ingredients + 2)
 
                     if self.indicate_successful_delivery:
                         num_layers += 1
